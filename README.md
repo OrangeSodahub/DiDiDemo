c3c9171c2165ec45e234126b3559d705fbc7d5fa
YangXiuyu <gzzyyxy@gmail.com>
2023-03-03 23:38:01 +0800

    det and rec demo

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # DiDiDemo
 
+### Build dataset
+```bash
+python dataset/create_dataset.py
+```
+
+### Evaluation on CCPD dataset
+recognition:
+```bash
+python tools/eval.py -c ../models/rec_ppocr_v3_finetune/config.yml -o \
+    Global.pretrained_model=../models/rec_ppocr_v3_finetune/best_accuracy.pdparams \
+    Eval.dataset.data_dir=/home/zonlin/PaddlePaddle/DiDiDemo/data/CCPD_processed \
+    Eval.dataset.label_file_list=[/home/zonlin/PaddlePaddle/DiDiDemo/data/CCPD_processed/test/rec.txt]
+```
+
+### End-to-end inference
+```bash
+python tools/infer/predict_system.py \
+    --det_model_dir=output/CCPD/det/infer/ \
+    --rec_model_dir=output/CCPD/rec/infer/ \
+    --image_dir="/home/aistudio/data/CCPD2020/ccpd_green/test/04131106321839081-92_258-159&509_530&611-527&611_172&599_159&509_530&525-0_0_3_32_30_31_30_30-109-106.jpg" \
+    --rec_image_shape=3,48,320
+```
+
+### Demo
+```bash
+python demo.py
+```
+
 ### Evalution on CCPD dataset [[logs]](https://github.com/OrangeSodahub/DiDiDemo/blob/master/backup_test.log):
 ```
 total 5006 precision 0.7843445008846527 precision_ 0.8725396825396825
--- demo.py
+++ demo.py
@@ -3,14 +3,17 @@ import cv2
 import yaml
 import argparse
 import logging
+import time
+import numpy as np
+from PIL import Image
 
-import paddlehub as hub
-from paddleocr import PaddleOCR, draw_ocr
+from utils import init_args, draw_ocr_box_txt
+from PaddleOCR.tools.infer.predict_system import TextSystem
 
-def run(config: dict, local: bool = False, save_results: bool = False, print_results: bool = False, use_ip_camera: bool = False):
+def run(config: dict, args):
     # TODO: support not save results
     save_dir = config['DATASETS']['save_results']
-    if local:
+    if args.local:
         imgs_folder_path = config['DATASETS']['test_folder']
         img_paths = os.listdir(imgs_folder_path)
         images = [cv2.imread(os.path.join(imgs_folder_path, img_path)) for img_path in img_paths]
@@ -18,50 +21,66 @@ def run(config: dict, local: bool = False, save_results: bool = False, print_res
 
     # Default to usb camera
     url = 0
-    if use_ip_camera:
+    if args.ip_camera:
         url = config['IP_CAMERA']['url']
     cap = cv2.VideoCapture(url)
 
     logging.info("======> Loading model ")
-    ocr = hub.Module(name=config['MODEL']['name'])
+    infer_sys = TextSystem(args)
+    font_path = args.vis_font_path
+    drop_score = args.drop_score
+    # warm up 10 times
+    if args.warmup:
+        img = np.random.uniform(0, 255, [640, 640, 3]).astype(np.uint8)
+        for _ in range(10):
+            infer_sys(img)
 
+    total_time = 0
+    cpu_mem, gpu_mem, gpu_util = 0, 0, 0
+    count = 0
+    fps = 0
+    start_time = time.time()
     logging.info("======> Running start ")
-    while(cap.isOpened()):  
-        ret, frame = cap.read()
-        images = [frame]
-        print(type(images))
-        print(type(images[0]))
-        print(images[0].shape)
-        results = ocr.recognize_text(
-                            images=images,
-                            use_gpu=True,
-                            output_dir=save_dir,
-                            visualization=True,
-                            box_thresh=0.5,
-                            text_thresh=0.5)
+    while(cap.isOpened()):
+        ret, image = cap.read()
+        # image = cv2.imread('/home/zonlin/PaddlePaddle/DiDiDemo/data/CCPD2020/01d3-kcunqze6941645.jpg')
+        dt_boxes, rec_res, time_dict = infer_sys(image)
+        count += 1
+        elaps = time.time() - start_time
+        total_time += elaps
+        fps = count / total_time
 
-        if print_results:
-            for result in results:
-                data = result['data']
-                save_path = result['save_path']
-                for infomation in data:
-                    print('text: ', infomation['text'], '\nconfidence: ', infomation['confidence'],
-                                        '\ntext_box_position: ', infomation['text_box_position'])
-        cv2.imshow('frame',frame)
+        if args.print:
+            for text, score in rec_res:
+                print("{}, {:.3f}".format(text, score))
+
+        # visualization
+        image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        boxes = dt_boxes
+        txts = [rec_res[i][0] for i in range(len(rec_res))]
+        scores = [rec_res[i][1] for i in range(len(rec_res))]
+
+        draw_img = draw_ocr_box_txt(image, boxes, txts, scores,
+                        drop_score=drop_score, font_path=font_path)
+
+        cv2.imshow('results', np.array(draw_img))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cap.release()  
+    cap.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Demo run arg parser')
+    parser: argparse.ArgumentParser = init_args()
     parser.add_argument("--config", help="path to config file", metavar="FILE", required=False, default= './configs/base.yaml')
     parser.add_argument("--local", help="whether to run offline", action='store_true', required=False)
     parser.add_argument("--save", help="whether to save the results", action='store_true', required=False)
     parser.add_argument("--print", help="whether to print the results in terminal", action='store_true', required=False)
     parser.add_argument("--ip_camera", help="whether to use the ip camera", action='store_true', required=False)
+    # End-to-end inference
+    parser.add_argument("--det_model_dir", type=str, default='./models/det_ppocr_v3_finetune/infer')
+    parser.add_argument("--rec_model_dir", type=str, default='./models/rec_ppocr_v3_finetune/infer')
     args = parser.parse_args()
 
     with open(args.config, 'r') as f:
@@ -70,4 +89,4 @@ if __name__ == '__main__':
         except:
             raise FileNotFoundError
     
-    run(config, args.local, args.save, args.print, args.ip_camera)
+    run(config, args)
